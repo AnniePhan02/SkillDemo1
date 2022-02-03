bb016b58451b58b49b0f96da15f9a523a71dd662
AnniePhan02 <a5phan@ucsd.edu>
2022-02-02 19:13:00 -0800

    passing test

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void multiply() {
-        assertEquals(1, SkillDemo.multiply(2, 3)); //should fail
+        assertEquals(6, SkillDemo.multiply(2, 3)); //should pass the JUnit test
     }
 }
